0ee6595a21bc4a0734963297fabf8f640aa00ace
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-11 14:16:43 -0600

    basic styles

--- reducertodo/src/components/List.js
+++ reducertodo/src/components/List.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ListItem from './ListItem';
 
 const List = props => (
-  <div>
-    <div>{props.state.map((item) => {
-      return <ListItem key={item.id} item={item} handleComplete={props.handleComplete}/>;
-    })}
-    </div>
+  <div className='listContain'>{props.state.map((item) => {
+    return <ListItem key={item.id} item={item} handleComplete={props.handleComplete}/>;
+  })}
   </div>
 )
 
